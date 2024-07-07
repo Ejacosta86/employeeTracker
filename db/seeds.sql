38e1bb2344401f7be9307b8133be93f5186f63e7
ejacosta86 <ejacosta86@gmail.com>
2024-07-07 17:07:39 -0400

    seeds created

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,28 @@
+INSERT INTO departments (department_id)
+VALUES
+('Sale'),
+('Engineering'),
+('Finance'),
+('Legal');
 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Sales Lead', 65000, 1),
+('Salesperson', 50000, 1),
+('Lead Engineer', 100000, 2),
+('Software Engineer', 85000, 2),
+('Account Manager', 105000, 3),
+('Accountant', 85000, 3),
+('Legal Team Lead', 250000, 4),
+("Lawyer", 125000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Anna", "Wolff", 1, null),
+("Mike", "Duffey", 2, 1),
+("Micah", "Cox", 3, null),
+("Paula", "Keldsen", 4, 3),
+("Scott", "Okamoto", 5, null),
+("Jessica", "Song", 6, 5),
+("Erica", "Acosta", 7, null),
+("Jung", "Yoon", 8, 7);
