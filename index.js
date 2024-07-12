0105785b5d8097f27dcf7dd96778c74c99b59e5f
ejacosta86 <ejacosta86@gmail.com>
2024-07-11 20:38:50 -0400

    fixed view all emp issue

--- index.js
+++ index.js
@@ -41,11 +41,7 @@ prompt([
     JOIN department ON role.department = department.id`);
       break;
     case "View all employees":
-    viewTable(`SELECT e1.id, e1.first_name, e1.last_name, role.title AS title, role.salary, department.name AS department, CONCAT(e2.first_name, ' ', e2.last_name) AS manager
-              FROM employee e1 
-              JOIN role ON e1.role_id = role.id 
-              JOIN department ON role.department.id = department.id
-              LEFT JOIN employee e2 ON e1.manager_id = e2.id`);
+      viewAllEmployees();
       break;
     case "Add a department":
       addDepartment();
@@ -81,12 +77,12 @@ function viewTable(queue) {
   });
 }
 
-function viewEmployees() {
+function viewAllEmployees() {
   viewTable(`
     SELECT e1.id, e1.first_name, e1.last_name, role.title AS title, role.salary, department.name AS department, CONCAT(e2.first_name, ' ', e2.last_name) AS manager
     FROM employee e1 
     JOIN role ON e1.role_id = role.id 
-    JOIN department ON role.department_id = department.id
+    JOIN department ON role.department = department.id
     LEFT JOIN employee e2 ON e1.manager_id = e2.id
   `);
 }
